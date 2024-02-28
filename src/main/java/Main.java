d9b54764733f776bbb2123be3c5f61fc8a06fc1d
Thiago Bandeira <thiago@lar.ifce.edu.br>
2024-02-28 18:37:20 -0300

    feat: criação da entidade Usuário - v1

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Usuario usuario = new Usuario("thiago@gmail.cO", "123456");
+
+        System.out.println(usuario.senha());
+        System.out.println(usuario.email());
+
+
+    }
+}
